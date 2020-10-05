3539bcec1a1c0dd408141064ae5f6bb3d42b4a9a
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2020-10-05 22:35:31 +0200

    Add linux(day).it style

--- provisioning/files/nginx-site-files/index.php
+++ provisioning/files/nginx-site-files/index.php
@@ -101,11 +101,15 @@ if(isset($_POST) && isset($_POST['nome']) && isset($_POST['email']) && isset($_P
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<link href="https://www.linux.it/shared/index.php?f=main.css" rel="stylesheet" type="text/css">
 	<link rel="stylesheet" href="index.css">
 	<title>Torneo di Teeworlds</title>
 </head>
 <body>
+<div id="header">
 <h1>Torneo di Teeworlds (qui si può mettere un'immagine)</h1>
+</div>
+<div class="container mt-3">
 <ul class="nav nav-tabs mb-3" role="tablist">
 	<li class="nav-item" role="presentation">
 		<a class="nav-link <?= $register ? '' : 'active' ?>" id="torneo-tab" data-toggle="tab" href="#torneo" role="tab" aria-controls="torneo" aria-selected="<?= $register ? 'false' : 'true' ?>">Torneo</a>
@@ -295,7 +299,7 @@ JOIN players AS d ON killed = d.password
 		<?php endif; ?>
 	</div>
 </div>
-
+</div>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
